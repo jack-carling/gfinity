9f53ed0fbf2e6149f90dce1220c299f9289a1ecf
jack-carling <jack@carling.se>
2022-02-01 21:08:41 +0100

    Added title
    • Function to render possessive case correctly
    • Fixed background image (caused overflow)
    • Added font styles and icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
 	<main>
-		<img
-			class="background-image"
-			src="@/assets/images/fifa19-ronaldo-fg-lg.png"
-			alt=""
-		/>
+		<div class="background-image"></div>
+		<section class="header flex mx-4 py-4">
+			<img
+				src="@/assets/images/gf-arrow-left.svg"
+				class="cursor-pointer"
+				alt=""
+			/>
+			<span class="callout-medium-dark ml-8">Matchmaking Lobby</span>
+		</section>
+		<section class="title flex p-4">
+			<h1 class="title-large-dark m-0">{{ renderLobbyText() }}</h1>
+			<img src="@/assets/images/gf-lock.svg" class="ml-4" alt="lock" />
+		</section>
 	</main>
 </template>
 
@@ -14,15 +22,35 @@ export default {
 		return {
 			title: 'Gfinity - Matchmaking Lobby'
 		}
+	},
+	data() {
+		return {
+			user: 'steveroesler'
+		}
+	},
+	methods: {
+		renderLobbyText() {
+			const user = this.user
+			const s = user.charAt(user.length - 1) === 's' ? "'" : "'s"
+			return user + s + ' lobby'
+		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
-img.background-image {
+div.background-image {
 	position: absolute;
-	top: -310px;
-	right: -175px;
+	width: 375px;
+	height: 696px;
+	top: 0;
+	right: 0;
+	background-image: url('~@/assets/images/fifa19-ronaldo-fg-lg.png');
+	background-position: 170px -310px;
+	background-repeat: no-repeat;
 	z-index: -999;
+}
+section.header {
+	border-bottom: 1px solid #cfd1c7;
 }
 </style>
