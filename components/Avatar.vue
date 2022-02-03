6f8f7a68e72ee141473e5d3955ca7b599e7949a9
jack-carling <jack@carling.se>
2022-02-03 14:40:44 +0100

    Update search-page
    • Moved avatar into component to reuse for next page
    • Added count component to page
    • Added menu-bar and refactored meta-head tag code to reuse for 'Searching...' text
    • Added timer counter
    • Random number between 10-15 will redirect user to next page

---
+++ components/Avatar.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="avatar">
+		<img src="@/assets/images/gf-avatar-01.png" alt="" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+div.avatar {
+	border: 2px solid #fff;
+	border-radius: 50%;
+	padding: 5px;
+	position: relative;
+	img {
+		width: 48px;
+		border-radius: 50%;
+	}
+	&:after {
+		content: '';
+		border: 2px solid #fff;
+		border-radius: 50%;
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		animation: ping 2s cubic-bezier(0, 0, 0.2, 1) infinite;
+	}
+}
+
+@keyframes ping {
+	75%,
+	100% {
+		transform: scale(2);
+		opacity: 0;
+	}
+}
+</style>
--- pages/search.vue
+++ pages/search.vue
@@ -15,26 +15,57 @@
 				</p>
 				<div class="info flex items-center ml-4">
 					<span class="caption-small-light">Lobby</span>
-					<div class="avatar">
-						<img src="@/assets/images/gf-avatar-01.png" alt="" />
+					<Avatar />
+				</div>
+				<div class="info flex items-center ml-4">
+					<div>
+						<div class="rectangle"></div>
 					</div>
+					<Counter />
 				</div>
 			</section>
+		</section>
+		<section class="menu flex items-center justify-between px-8">
+			<span class="callout-small-light-bold">{{ searching }}</span>
+			<div class="flex">
+				<span class="callout-small-light-reg">{{ time }}</span>
+				<NuxtLink to="/">
+					<img
+						class="ml-8 cursor-pointer"
+						src="@/assets/images/gf-close.svg"
+						alt=""
+					/>
+				</NuxtLink>
+			</div>
 		</section>
 	</main>
 </template>
 
 <script>
 import Back from '~/components/Back.vue'
+import Avatar from '~/components/Avatar.vue'
+import Counter from '~/components/Counter.vue'
 
 export default {
 	layout: 'lobby',
 	components: {
-		Back
+		Back,
+		Avatar,
+		Counter
 	},
 	data() {
 		return {
-			title: 'Gfinity - Searching'
+			searching: 'Searching',
+			timer: 0,
+			randomTime: this.randomInterval(10, 15)
+		}
+	},
+	computed: {
+		title() {
+			return 'Gfinity - ' + this.searching
+		},
+		time() {
+			return new Date(this.timer * 1000).toTimeString().substring(3, 9)
 		}
 	},
 	head() {
@@ -44,10 +75,16 @@ export default {
 	},
 	mounted() {
 		setInterval(() => {
-			if (this.title === 'Gfinity - Searching...')
-				return (this.title = 'Gfinity - Searching')
-			this.title += '.'
+			this.timer++
+			if (this.searching === 'Searching...')
+				return (this.searching = 'Searching')
+			this.searching += '.'
 		}, 1000)
+	},
+	methods: {
+		randomInterval(min, max) {
+			return Math.floor(Math.random() * (max - min + 1) + min)
+		}
 	}
 }
 </script>
@@ -74,38 +111,25 @@ section.content {
 	z-index: 1;
 }
 div.info {
-	padding-bottom: 3rem;
-	span {
+	padding-bottom: 2rem;
+	> :first-child {
 		width: 75px;
 	}
-	div.avatar {
-		border: 2px solid #fff;
-		border-radius: 50%;
-		padding: 5px;
-		position: relative;
-		img {
-			width: 48px;
-			border-radius: 50%;
-		}
-		&:after {
-			content: '';
-			border: 2px solid #fff;
-			border-radius: 50%;
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			animation: ping 2s cubic-bezier(0, 0, 0.2, 1) infinite;
-		}
+	div.rectangle {
+		height: 2px;
+		width: 12px;
+		background-color: #fff;
+	}
+	&:last-child {
+		padding-bottom: 6rem;
 	}
 }
-
-@keyframes ping {
-	75%,
-	100% {
-		transform: scale(2);
-		opacity: 0;
-	}
+section.menu {
+	width: calc(100% - 80px);
+	height: 90px;
+	background-color: #1b1b1b;
+	position: relative;
+	top: -45px;
+	left: 80px;
 }
 </style>
